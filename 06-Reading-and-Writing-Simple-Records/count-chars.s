75930e7037fb743cb4cdc352c5205366b74c8b3a
freebootin <patrick@freebootin.com>
2022-10-25 21:29:39 -0700

    initial commit

---
+++ 06-Reading-and-Writing-Simple-Records/count-chars.s
@@ -0,0 +1,35 @@
+.type count_chars, @function
+.globl count_chars
+
+# This is where our one parameter is on the stack
+.equ ST_STRING_START_ADDRESS, 8
+
+count_chars:
+	pushl %ebp
+	movl %esp, %ebp
+
+	# Counter starts at zero
+	movl $0, %ecx
+
+	# Starting address of data
+	movl ST_STRING_START_ADDRESS(%ebp), %edx
+
+count_loop_begin:
+	# Grab the current character
+	movb (%edx), %al
+	# Is it null?
+	cmpb $0, %al
+	# If yes, we're done
+	je count_loop_end
+	# Otherwise, increment the counter and the pointer
+	incl %ecx
+	incl %edx
+	# Go back to the beginning of the loop
+	jmp count_loop_begin
+
+count_loop_end:
+	# We're done. Move the count into %eax and return.
+	movl %ecx, %eax
+	
+	popl %ebp
+	ret
